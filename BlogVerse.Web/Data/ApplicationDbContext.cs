933d98e30888843938346f1cfb6e846cba963a36
xtsumit <mahadik.sumit97@gmail.com>
2024-04-15 12:35:40 +0530

    BlogPost And Tags Table Created.

---
+++ BlogVerse.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogVerse.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogVerse.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
